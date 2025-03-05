3ff1795c701b6488a39422b638aed52ec72245bc
Miroslav Plavec <miroslav.plav@gmail.com>
2025-03-05 11:26:48 +0100

    Swapping cronn and scheduled for controller whihc will be called by google app scheduler

--- src/main/java/com/example/todoapp/TodoappApplication.java
+++ src/main/java/com/example/todoapp/TodoappApplication.java
@@ -2,10 +2,8 @@ package com.example.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class TodoappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/todoapp/checkdeadline/DeadlineRepositry.java
+++ src/main/java/com/example/todoapp/checkdeadline/DeadlineRepositry.java
@@ -1,11 +1,11 @@
-package com.example.todoapp.scheduler;
+package com.example.todoapp.checkdeadline;
 
 import com.example.todoapp.task.Task;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.Repository;
 
-public interface SchedulerTaskRepository extends Repository<Task, Integer> {
+public interface DeadlineRepositry extends Repository<Task, Integer> {
 
     @Modifying
     @Query("UPDATE tasks SET after_deadline=true WHERE deadline_date < :today")
